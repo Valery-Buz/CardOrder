4685c48b8959604e684bb3ea2603817dc1b35943
Valery Buz <Ajven@yandex.ru>
2022-02-24 01:30:16 +0300

    undo changes

--- src/test/java/ru/netology/order/CardOrderTest.java
+++ src/test/java/ru/netology/order/CardOrderTest.java
@@ -9,19 +9,18 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import io.github.bonigarcia.wdm.WebDriverManager;
+
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class CardOrderTest {
 
-
     private WebDriver driver;
 
     @BeforeAll
     public static void setUpAll() {
-        WebDriverManager.chromedriver().setup();
+
         System.setProperty("webdriver.chrome.driver", "./driver/win/chromedriver.exe");
     }
 
